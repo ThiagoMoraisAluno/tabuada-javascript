d0813b3c62809d3e9e0abfa407de357435519081
Thiago Morais <thiagomoraiscordeiro@outlook.com>
2024-02-29 14:54:58 -0300

    finalizando o projeto

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,36 @@
-console.log('Teste')
+//Seleção de elementos
+const multiplicationForm = document.querySelector('#multiplication-form');
+const numberInput = document.querySelector('#number');
+const multiplicationInput = document.querySelector('#multiplicator');
+
+const multiplicationTable = document.querySelector('#multiplication-operations');
+//Funções
+const createTable = (number, multiplicatorNumber)=>{
+    multiplicationTable.innerHTML = ''
+
+    for(i = 1; i <= multiplicatorNumber; i++){
+        const result = number * i;
+        console.log(result)
+
+        const template = `<div class = 'row'> 
+        <div class = 'operation'>${number} x ${i} =</div>
+        <div class = 'result'>${result}</div>
+        </div>`
+
+        const parse = new DOMParser()
+        const htmlTemplate = parse.parseFromString(template,'text/html');
+
+        const row = htmlTemplate.querySelector('.row');
+        multiplicationTable.appendChild(row);
+    }
+}
+//Eventos
+multiplicationForm.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const multiplicationNumber = +numberInput.value;
+    const multiplicatorNumber = +multiplicationInput.value;
+
+    if(!multiplicationNumber || !multiplicatorNumber)return;
+
+    createTable(multiplicationNumber, multiplicatorNumber);
+});
